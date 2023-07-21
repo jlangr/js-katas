14b8c389a555ec22a9d4180f48427e9c45f26696
Merlijn Tishauser <merlijn@industriallogic.com>
2023-07-21 18:03:46 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/checknumbers.test.js

--- src/checknumbers.js
+++ src/checknumbers.js
@@ -1,22 +1,53 @@
-const conversions = {0: 'zero', 1: 'one', 2: 'two', 10: 'ten', 13: 'thirteen', 16: 'sixteen', 17: 'seventeen'}
+const conversions = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen'
+}
 const tens = {2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty', 6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety'}
 
-export const checkNumber = number => {
-  if (number < 20)
-    return conversions[number]
-
-  if (number < 100) {
+function numbersBetween20and100(number) {
     const digit = Math.floor(number / 10)
     const remaining = number % 10
     return tens[digit] + (remaining > 0 ? '-' + conversions[remaining] : '')
-  }
+}
 
-  const remaining = number % 100
-  let s = 'one hundred'
+export const checkNumber = number => {
+    if (number < 20)
+        return conversions[number]
 
-  if (remaining > 0) {
-    s += ' ' + conversions[remaining]
-  }
 
-  return s
+    if (number < 100) {
+        return numbersBetween20and100(number);
+    }
+
+    const remaining = number % 100
+    let s = 'one hundred'
+
+    if (remaining > 20) {
+        s += ' ' + numbersBetween20and100(remaining);
+        return s;
+    }
+
+    if (remaining > 0) {
+        s += ' ' + conversions[remaining]
+    }
+
+    return s
 }
--- src/checknumbers.test.js
+++ src/checknumbers.test.js
@@ -1,37 +1,30 @@
 import {checkNumber} from './checknumbers'
 
 describe('check numbers', () => {
-  it('converts 1 to string equivalent', () => {
-    expect(checkNumber(1)).toBe('one')
-  })
+  it.each([
+    [1, 'one'],
+    [2, 'two'],
+    [3, 'three'],
+    [4, 'four'],
+    [5, 'five'],
+    [6, 'six'],
+    [7, 'seven'],
+    [8, 'eight'],
+    [9, 'nine'],
+    [10, 'ten'],
+    [11, 'eleven'],
+    [12, 'twelve'],
+    [13, 'thirteen'],
+    [14, 'fourteen'],
+    [15, 'fifteen'],
+    [16, 'sixteen'],
+    [17, 'seventeen'],
+    [18, 'eighteen'],
+    [19, 'nineteen']
+  ])('converts %p expecting %p for numbers < 20', (number, result) => {
+    expect(checkNumber(number)).toBe(result)
+  });
 
-  it('converts 0 to string equivalent', () => {
-    expect(checkNumber(0)).toBe('zero')
-  })
-
-  it('converts 2 to string equivalent', () => {
-    expect(checkNumber(2)).toBe('two')
-  })
-
-  it('converts 10 to string equivalent', () => {
-    expect(checkNumber(10)).toBe('ten')
-  })
-
-  it('converts 16 to string equivalent', () => {
-    expect(checkNumber(16)).toBe('sixteen')
-  })
-
-  it('converts 17 to string equivalent', () => {
-    expect(checkNumber(17)).toBe('seventeen')
-  })
-
-  it('converts 20 to string equivalent', () => {
-    expect(checkNumber(20)).toBe('twenty')
-  })
-
-  it('converts 21 to string equivalent', () => {
-    expect(checkNumber(21)).toBe('twenty-one')
-  })
 
   it.each([
     [13, 'thirteen'],
@@ -39,8 +32,9 @@ describe('check numbers', () => {
     [100, 'one hundred'],
     [101, 'one hundred one'],
     [113, 'one hundred thirteen'],
-    [123, 'one hundred twenty-three']
-  ])('converts %p expecting %p', (number, result) => {
+    [123, 'one hundred twenty-three'],
+    [254, 'two hundred fifty-four']
+  ])('converts %p expecting %p > 10', (number, result) => {
     expect(checkNumber(number)).toBe(result)
   });
 })
