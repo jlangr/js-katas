565119d532a992c7507d3d1fed80b5ebbc1a03f4
Merlijn Tishauser <merlijn@industriallogic.com>
2023-07-21 17:03:02 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/checknumbers.js

--- src/checknumbers.js
+++ src/checknumbers.js
@@ -1,13 +1,18 @@
 const conversions = {0: "zero", 1: "one", 2: "two", 10: "ten", 16: "sixteen", 17: "seventeen"}
+const tens = {2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty', 6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety'}
 export const checkNumber = number => {
     // can be divided by ten?
     // if not < 10
     // if so > tens as prefix, remainder as number
+    if (number >= 20) {
+        const digit = Math.floor(number / 10);
+        const remaining = number % 10;
+        if (remaining === 0) {
+            return tens[digit]
+        }
 
-    if (number === 20)
-        return "twenty"
-    if (number > 20)
-        return "twenty-" + conversions[number % 20]
+        return tens[digit] + '-' + conversions[remaining];
+    }
 
     return conversions[number]
 }
